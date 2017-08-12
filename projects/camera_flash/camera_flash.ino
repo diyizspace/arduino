e57a7bdbd42911145a3507c810e4046e978078b1
diyiz.space <diyiz.space@gmail.com>
2017-08-12 17:26:36 +0700

    update

--- projects/camera_flash/camera_flash.ino
+++ projects/camera_flash/camera_flash.ino
@@ -3,8 +3,6 @@
 #include <Adafruit_GFX.h>
 #include <Adafruit_PCD8544.h>
 
-Adafruit_PCD8544 lcd = Adafruit_PCD8544(2, 3, 4, 5, 6);
-
 class Menu {
     String title;
     int value;
@@ -22,13 +20,46 @@ class Menu {
       max_value = _max_value;
       step_value = _step_value;
     };
+
+    void handle(String input) {
+      if (input == "0010") {
+        int new_value = value - step_value;
+        if (new_value <= min_value) {
+          value = min_value;
+        } else {
+          value = new_value;
+        }
+      } else if (input == "0001") {
+        int new_value = value + step_value;
+        if (new_value >= max_value) {
+          value = max_value;
+        } else {
+          value = new_value;
+        }
+      }
+    };
+
+
+    String get_title() {
+      return title;
+    };
+
+    int get_value() {
+      return value;
+    };
+
+    String get_value_as_string() {
+      return String(value);
+    }
 };
 
 class Menus {
+    enum Mode {CONFIG = 0, READY = 1} mode;
     byte pin_up;
     byte pin_down;
     byte pin_left;
     byte pin_right;
+    int current_menu = 0;
     int menu_length;
     Menu menus[];
   public:
@@ -39,59 +70,69 @@ class Menus {
       pin_right = _pin_right;
     };
     void setup() {
+      if (get_input() == "1111") {
+        mode = READY;
+      } else {
+        mode = CONFIG;
+      }
       menu_length = 4;
       menus[0] = Menu("Brightness", 150, 150, 100, 255, 5);
       menus[1] = Menu("Pre-delay", 0, 0, 0, 1000, 50);
       menus[2] = Menu("Duration", 250, 250, 125, 1000, 125);
       menus[3] = Menu("LCD brightness", 60, 60, 40, 70, 5);
     };
+
+    Menu get_current_menu () {
+      return menus[current_menu]  ;
+    };
     void handle() {
-      if (input != lastButtonValue) {
-        lastDebounceTime = millis();
-      }
-      if ((millis() - lastDebounceTime) > debounceDelay) {
-        return;
-      }
-      lastButtonValue = input;
-      if (input == 1) {
-        if (currentMenu > 0) {
-          currentMenu -= 1;
+      String input = get_input();
+      if (input == "1000") {
+        if (current_menu > 0) {
+          current_menu -= 1;
         }
-      }
-      if (input == 2) {
-        if (currentMenu < menuLength) {
-          currentMenu += 1;
+      } else if (input == "0100") {
+        if (current_menu < menu_length - 1) {
+          current_menu += 1;
         }
+      } else {
+        menus[current_menu].handle(input);
       }
-      if ( input == 5) {
-        menus[currentMenu] += menuStep[currentMenu];
-      }
-      if ( input == 9) {
-        menus[currentMenu] -= menuStep[currentMenu];
-      }
-      show(String(menuTitles[currentMenu]), 1, String(menus[currentMenu]), 1);
       delay(50);
+    };
 
-    }
-    String input() {
+    String get_input() {
       return String(digitalRead(pin_up) + digitalRead(pin_down) + digitalRead(pin_left) + digitalRead(pin_right));
-    }
-};
+    };
 
-boolean configMode = false;
+    boolean is_config_mode() {
+      return mode == CONFIG;
+    };
+};
+Adafruit_PCD8544 lcd = Adafruit_PCD8544(2, 3, 4, 5, 6);
+
+Menus menus = Menus(10, 16, 15, 14);
+
 
 void setup() {
   lcd.begin();
   lcd.setContrast(60);
-
-  show("DIY LED FLASH", 1, "BOOTING ...", 1);
+  show("DIYIZ FLASH", 1, "BOOTING ...", 1);
+  Serial.begin(9600);
+  Serial.println("Start...");
+  menus.setup();
   delay(1000);
 }
 
 void loop() {
-  if (configMode) {
-    show("", 1, "", 1);
-    return;
+  if (menus.is_config_mode()) {
+    menus.handle();
+    Menu m = menus.get_current_menu();
+    show(m.get_title(), 1, m.get_value_as_string(), 1);
+  } else {
+    show("Stand by", 1, "...", 1);
+    Serial.println("standby");
+    delay(1000);
   }
 }
 
