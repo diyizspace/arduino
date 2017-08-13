599dcf975dfead029dcb7c36788166ec7540c3c0
diyiz.space <diyiz.space@gmail.com>
2017-08-13 13:34:07 +0700

    update

--- projects/camera_flash/camera_flash.ino
+++ projects/camera_flash/camera_flash.ino
@@ -21,21 +21,20 @@ class Menu {
       step_value = _step_value;
     };
 
-    void handle(String input) {
-      if (input == "0010") {
-        int new_value = value - step_value;
-        if (new_value <= min_value) {
-          value = min_value;
-        } else {
-          value = new_value;
-        }
-      } else if (input == "0001") {
-        int new_value = value + step_value;
-        if (new_value >= max_value) {
-          value = max_value;
-        } else {
-          value = new_value;
-        }
+    void descrease() {
+      int new_value = value - step_value;
+      if (new_value <= min_value) {
+        value = min_value;
+      } else {
+        value = new_value;
+      }
+    }
+    void increase() {
+      int new_value = value + step_value;
+      if (new_value >= max_value) {
+        value = max_value;
+      } else {
+        value = new_value;
       }
     }
 
@@ -58,9 +57,14 @@ class Menus {
     int pin_down;
     int pin_left;
     int pin_right;
-    int current_menu = 0;
+    int current_menu_index = 0;
     int menu_length = 4;
-    Menu menus[4];
+    Menu menus[4] = {
+      Menu("Brightness", 150, 150, 100, 255, 5),
+      Menu("Pre-delay", 0, 0, 0, 1000, 50),
+      Menu("Duration", 250, 250, 125, 1000, 125),
+      Menu("LCD brightness", 60, 60, 40, 70, 5)
+    };
   public:
     Menus(int _pin_up, int _pin_down, int _pin_left, int _pin_right) {
       pin_up = _pin_up;
@@ -69,38 +73,41 @@ class Menus {
       pin_right = _pin_right;
     }
     void setup() {
-      if (get_input() == "1111") {
+      if (get_input() == 15) {
         mode = READY;
       } else {
         mode = CONFIG;
       }
-      //      menus[0] = Menu("Brightness", 150, 150, 100, 255, 5);
-      //      menus[1] = Menu("Pre-delay", 0, 0, 0, 1000, 50);
-      //      menus[2] = Menu("Duration", 250, 250, 125, 1000, 125);
-      //      menus[3] = Menu("LCD brightness", 60, 60, 40, 70, 5);
     }
 
-    Menu get_current_menu () {
-      return menus[current_menu]  ;
-    };
-    void handle() {
-      String input = get_input();
-      if (input == "1000") {
-        if (current_menu > 0) {
-          current_menu -= 1;
-        }
-      } else if (input == "0100") {
-        if (current_menu < menu_length - 1) {
-          current_menu += 1;
-        }
-      } else {
-        menus[current_menu].handle(input);
+    Menu get_menu(int position) {
+      return menus[position]  ;
+    }
+
+    Menu get_current_menu() {
+      return get_menu(current_menu_index);
+    }
+
+    int get_current_menu_index() {
+      return current_menu_index;
+    }
+
+    void previous_menu() {
+      current_menu_index -= 1;
+      if (current_menu_index < 0) {
+        current_menu_index = menu_length - 1;
       }
-      delay(50);
+    }
+    void next_menu() {
+      current_menu_index += 1;
+      if (current_menu_index >= menu_length) {
+        current_menu_index = 0;
+      }
     }
 
-    String get_input() {
-      return String(digitalRead(pin_up) + digitalRead(pin_down) + digitalRead(pin_left) + digitalRead(pin_right));
+
+    int get_input() {
+      return 8 * digitalRead(pin_up) + 4 * digitalRead(pin_down) + 2 * digitalRead(pin_left) + digitalRead(pin_right);
     }
 
     boolean is_config_mode() {
@@ -109,28 +116,51 @@ class Menus {
 };
 Adafruit_PCD8544 lcd = Adafruit_PCD8544(2, 3, 4, 5, 6);
 
-Menus menus(10, 16, 15, 14);
+Menus menus(10, 16, 14, 15);
 
+int lastButtonValue = 0;
+unsigned long lastDebounceTime = 0;
+unsigned long debounceDelay = 50;
 
 void setup() {
   lcd.begin();
   lcd.setContrast(60);
   show("DIYIZ.SPACE", 1, "BOOTING ...", 1);
+  menus.setup();
   Serial.begin(9600);
-  Serial.println("Start...");
-  //  menus.setup();
-
-  delay(1000);
+  Serial.println("ready...");
+  delay(500);
 }
 
 void loop() {
   if (menus.is_config_mode()) {
-    menus.handle();
-    Menu m = menus.get_current_menu();
-    show(m.get_title(), 1, m.get_value_as_string(), 1);
+    handle_config();
   } else {
+    Serial.println("stand by");
+    delay(1000);
     show("Stand by", 1, "...", 1);
   }
+}
+
+void handle_config() {
+  int input = menus.get_input();
+  if (input == 0) {
+    return 0;
+  } else if (input == 8) {
+    menus.previous_menu();
+  } else if (input == 4) {
+    menus.next_menu();
+  } else if (input == 2) {
+    Menu m = menus.get_current_menu();  
+    m.descrease();
+  } else if (input == 1) {
+    Menu m = menus.get_current_menu();  
+    m.increase();
+  }
+  Menu m = menus.get_current_menu();
+  show(m.get_title(), 1, m.get_value_as_string(), 1);
+  Serial.println(menus.get_current_menu_index() + "|" + m.get_value());
+  delay(250);
 }
 
 void show(String line1, int font1, String line2, int font2) {
